ffa503dfd38476ead39aa8af8a88e48e617bba9b
Michael Dannhardt <mdannhardt@dastec.com>
2023-10-24 13:52:22 -0400

    Attempt to intercept MMS text. Not working. SmsListener now MsgListener.

---
+++ MorseRingerApp/src/main/java/mike/dannhardt/morseCodeRinger/MmsReceiver.java
@@ -0,0 +1,54 @@
+package mike.dannhardt.morseCodeRinger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.support.v4.content.LocalBroadcastManager;
+import android.telephony.SmsMessage;
+
+public class MmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent localIntent = new Intent(Constants.SMS_RCVD)
+                .putExtra(Constants.EXTRA_FROM_NUMBER, "1")
+                .putExtra(Constants.EXTRA_SMS_DATA, "??");
+        LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+    }
+        /*
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            SmsMessage[] messages = new SmsMessage[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+            }
+            String msgFrom, msgBody;
+            msgFrom = "";
+            msgBody = "";
+            for(int i=0; i< messages.length; i++){
+                msgFrom = messages[i].getOriginatingAddress();
+                msgBody = messages[i].getMessageBody();
+            }
+            if (messages.length>0) {
+                Intent localIntent = new Intent(Constants.SMS_RCVD)
+                        .putExtra(Constants.EXTRA_FROM_NUMBER, msgFrom)
+                        .putExtra(Constants.EXTRA_SMS_DATA, msgBody);
+                LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+            }
+        } else if (intent.getAction().equals("android.provider.Telephony.WAP_PUSH_RECEIVED")){
+            Intent localIntent = new Intent(Constants.SMS_RCVD)
+                    .putExtra(Constants.EXTRA_FROM_NUMBER, "1234")
+                    .putExtra(Constants.EXTRA_SMS_DATA, "wap");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+        } else if (intent.getAction().equals("android.provider.Telephony.MMS_RECEIVED")){
+            Intent localIntent = new Intent(Constants.SMS_RCVD)
+                    .putExtra(Constants.EXTRA_FROM_NUMBER, "11")
+                    .putExtra(Constants.EXTRA_SMS_DATA, "mms");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+        } else {
+            Intent localIntent = new Intent(Constants.SMS_RCVD)
+                    .putExtra(Constants.EXTRA_FROM_NUMBER, "1")
+                    .putExtra(Constants.EXTRA_SMS_DATA, "??");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+        }
+    } */
+}
--- MorseRingerApp/src/main/java/mike/dannhardt/morseCodeRinger/MsgReceiver.java
+++ MorseRingerApp/src/main/java/mike/dannhardt/morseCodeRinger/MsgReceiver.java
@@ -7,10 +7,10 @@ import android.provider.Telephony;
 import android.support.v4.content.LocalBroadcastManager;
 import android.telephony.SmsMessage;
 
-public class SmsListener extends BroadcastReceiver {
+public class MsgReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
             SmsMessage[] messages = new SmsMessage[0];
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
                 messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
@@ -28,6 +28,21 @@ public class SmsListener extends BroadcastReceiver {
                         .putExtra(Constants.EXTRA_SMS_DATA, msgBody);
                 LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
             }
+        } else if (intent.getAction().equals("android.provider.Telephony.WAP_PUSH_RECEIVED")){
+            Intent localIntent = new Intent(Constants.SMS_RCVD)
+                    .putExtra(Constants.EXTRA_FROM_NUMBER, "1234")
+                    .putExtra(Constants.EXTRA_SMS_DATA, "wap");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+        } else if (intent.getAction().equals("android.provider.Telephony.MMS_RECEIVED")){
+            Intent localIntent = new Intent(Constants.SMS_RCVD)
+                    .putExtra(Constants.EXTRA_FROM_NUMBER, "11")
+                    .putExtra(Constants.EXTRA_SMS_DATA, "mms");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+        } else {
+            Intent localIntent = new Intent(Constants.SMS_RCVD)
+                    .putExtra(Constants.EXTRA_FROM_NUMBER, "1")
+                    .putExtra(Constants.EXTRA_SMS_DATA, "??");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
         }
     }
 }
